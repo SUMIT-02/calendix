92025659a227b52d6af3571ecf0d5939ca174c8a
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-29 02:02:48 +0530

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Testing</div>;
+  return (
+    <main className="container">
+      <header className="flex gap-4 justify-between py-4 text-gray-600 font-light">
+        <div className="flex items-center gap-6">
+          <Link href={"/"} className="text-blue-600 text-xl font-bold">
+            Calendix
+          </Link>
+          <nav className="flex gap-4">
+            <Link href={"/features"}>Features</Link>
+            <Link href={"/about"}>About</Link>
+            <Link href={"/pricing"}>Pricing</Link>
+          </nav>
+        </div>
+        <nav>
+          <Link href={"/singIn"}>Features</Link>
+          <Link href={"/about"}>Get started</Link>
+        </nav>
+      </header>
+    </main>
+  );
 }
