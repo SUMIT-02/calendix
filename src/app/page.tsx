a51e588fe59c310af6da7131aaae10c670c0c69e
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-30 00:50:40 +0530

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
+import { CalendarDays } from "lucide-react";
 import Link from "next/link";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <main className="container">
-      <header className="flex gap-4 justify-between py-4 text-gray-600 font-light">
-        <div className="flex items-center gap-6">
-          <Link href={"/"} className="text-blue-600 text-xl font-bold">
-            Calendix
-          </Link>
-          <nav className="flex gap-4">
-            <Link href={"/features"}>Features</Link>
-            <Link href={"/about"}>About</Link>
-            <Link href={"/pricing"}>Pricing</Link>
-          </nav>
-        </div>
-        <nav>
-          <Link href={"/singIn"}>Features</Link>
-          <Link href={"/about"}>Get started</Link>
-        </nav>
-      </header>
+      <Header />
     </main>
   );
 }
