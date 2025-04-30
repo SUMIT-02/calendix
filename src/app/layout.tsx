5dc59b88014c24dda0029cc6d2ce4757d998e4a6
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-05-01 02:49:01 +0530

    homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const noto = Noto_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={noto.className}>{children}</body>
+      <body className={noto.className}>
+        <main className="container">
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
